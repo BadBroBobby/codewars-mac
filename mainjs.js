12486212ecc3f693c9e13cd18851dfd20ccf77de
Chris Vandborg <chrisvandborg@MacBook-Pro.local>
2023-03-06 21:40:38 +0100

    second today

--- mainjs.js
+++ mainjs.js
@@ -70,3 +70,24 @@ function uniTotal (string) {
     }
     return num
   }
+
+//8 kyu
+//simple calculator
+
+  function calculator(a,b,sign){
+    if(typeof a != "number" || typeof b != "number" ){
+      return "unknown value"
+    }
+    
+    if (sign == "+"){
+      return a + b
+    }else if( sign == "-"){
+      return a - b
+    }else if( sign == "*"){
+      return a * b
+    }else if( sign == "/"){
+      return a / b
+    }else{
+      return "unknown value"
+    }
+  }
